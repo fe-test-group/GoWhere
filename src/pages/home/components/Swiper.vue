4176f3b719fd7248a3d71542810ca5a7051d239a
rsl140 <rsl140@qq.com>
2019-07-14 23:48:32 +0800

    feat: 首页轮播图

---
+++ src/pages/home/components/Swiper.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="wrapper">
+    <swiper :options="swiperOption">
+      <swiper-slide v-for="(item, index) of list" :key="index">
+        <img class="swiper-img" :src="item.imgUrl" />
+      </swiper-slide>
+      <div class="swiper-pagination"  slot="pagination"></div>
+    </swiper>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeSwiper',
+  data () {
+    return {
+      swiperOption: {
+        pagination: '.swiper-pagination',
+        loop: true
+      },
+      list: [
+        {
+          imgUrl: 'https://source.qunarzz.com/site/images/wns/20190705_qunar_dujia_homepage_top_banner_1.jpg'
+        }, {
+          imgUrl: 'https://source.qunarzz.com/site/images/wns/2019704_dujia_homepage_top_banner_6.jpg'
+        }
+      ]
+    }
+  }
+}
+</script>
+<style lang="stylus" scoped>
+  .wrapper >>> .swiper-pagination-bullet-active
+    background: #fff
+  .wrapper
+    overflow: hidden
+    width: 100%
+    height: 0
+    padding-bottom: 31.25%
+    .swiper-img
+      width: 100%
+</style>
