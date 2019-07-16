d7121d8525fd5db2f11d2cd6e543f2270947c874
rsl140 <rsl140@qq.com>
2019-07-17 00:33:29 +0800

    feat: city add axios

--- src/pages/city/City.vue
+++ src/pages/city/City.vue
@@ -5,10 +5,34 @@
 </template>
 <script>
 import CityHeader from './components/Header'
+import axios from 'axios'
 export default {
   name: 'City',
   components: {
     CityHeader
+  },
+  data () {
+    return {
+      hotCities: {},
+      cities: []
+    }
+  },
+  methods: {
+    getCityInfo () {
+      axios.get('http://localhost:8080/static/mock/city.json')
+        .then(this.getCityInfoSucc)
+    },
+    getCityInfoSucc (res) {
+      res = res.date
+      if (res.ret && res.data) {
+        const data = res.data
+        this.hotCities = data.hotCities
+        this.cities = data.cities
+      }
+    }
+  },
+  mounted () {
+    this.getCityInfo()
   }
 }
 </script>
---
+++ src/pages/city/components/Search.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <div class="search">
+      <input v-model="keyword" class="search-input" type="text" placeholder="输入城市名或拼音" />
+    </div>
+    <div class="search-content" ref="search" v-show="keyword" >
+      <ul>
+        <li class="search-item border-bottom" v-for="item of list" :key="item.id" @click="handleCityClick(item.name)" >
+          {{item.name}}
+        </li>
+      </ul>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  name: 'CitySearch'
+}
+</script>
+<style lang="stylus" scoped>
+</style>
