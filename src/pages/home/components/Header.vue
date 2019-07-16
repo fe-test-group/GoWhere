14ebb9ad4577e579c3a1782718242a182460e7d9
rsl140 <rsl140@qq.com>
2019-07-17 00:22:25 +0800

    feat: add city&cityheader

--- src/pages/home/components/Header.vue
+++ src/pages/home/components/Header.vue
@@ -2,7 +2,7 @@
  * @Author: rsl
  * @Date: 2019-07-14
  * @LastEditors: rsl
- * @LastEditTime: 2019-07-15
+ * @LastEditTime: 2019-07-17
  * @Description: 首页头部组件
  -->
 <template>
@@ -14,10 +14,12 @@
       <span class="iconfont">&#xe632;</span>
       输入城市/景点/游玩主题
     </div>
-    <div class="header-right">
-      城市
-      <span class="iconfont arrow-icon">&#xe64a;</span>
-    </div>
+    <router-link to="/city">
+      <div class="header-right">
+        城市
+        <span class="iconfont arrow-icon">&#xe64a;</span>
+      </div>
+    </router-link>
   </div>
 </template>
 
@@ -31,7 +33,7 @@ export default {
   @import '~styles/varibles.styl'
   .header
     display: flex
-    line-height: 0.86rem
+    line-height: $headerHeight
     background: $bgColor
     color: #fff
     .header-left
